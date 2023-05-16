fe414b954e027818840f28c67292f906d869b7cb
franciscoG98 <franciscomanuel.gonzalez3@gmail.com>
2023-05-15 22:54:44 -0300

    edit task and get one task

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,19 +1,36 @@
+import { useEffect } from "react"
 import Navigation from "../components/Navigation"
 import { useForm } from 'react-hook-form'
-import { createTask, deleteTask } from "../api/tasks.api"
+import { createTask, deleteTask, updateTask, getTask } from "../api/tasks.api"
 import { useNavigate, useParams } from "react-router-dom"
 
 const TaskFormPage = () => {
 
-  const { register, handleSubmit, formState: { errors } } = useForm()
+  const { register, handleSubmit, formState: { errors }, setValue } = useForm()
 
   const navigate = useNavigate()
   const params = useParams()
 
-  const onSubmit = handleSubmit(async data => {
-    await createTask(data)
+  const onSubmit = handleSubmit(async (data) => {
+
+    if(params.id) {
+      await updateTask(params.id, data)
+    } else {
+      await createTask(data)
+    }
     navigate('/tasks')
   })
+
+  useEffect(() => {
+    async function loadTask() {
+      if(params.id) {
+        const res = await getTask(params.id)
+        setValue('title', res.data.title)
+        setValue('description', res.data.description)
+      }
+    }
+    loadTask()
+  }, [params.id, setValue])
 
     return (
       <>
